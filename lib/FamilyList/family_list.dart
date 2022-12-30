fcf457b55d8217521a651e44ab48e2c91b5eac10
PatlollaManisha <manishapatlolla27@gmail.com>
2022-12-30 10:59:40 +0530

    merge Changes

--- lib/FamilyList/family_list.dart
+++ lib/FamilyList/family_list.dart
@@ -178,7 +178,7 @@ class _familyListState extends State<familyList> {
 
     AppConstants.famMemId = selectedId ?? 0;
     print('app const is ${AppConstants.famMemId}');
-    fetchdata(selectedId ?? 0);
+    fetchdata(selectedId ?? 0,name);
    // fetchAllPresData(selectedId ?? 0);
   }
 
@@ -229,7 +229,7 @@ class _familyListState extends State<familyList> {
     });
   }
   */
-  fetchdata(int id) async {
+  fetchdata(int id, String name) async {
 
     // print('selected id is $id');
     await DatabaseHelper.instance.prescList('Prescription', id).then((value) {
